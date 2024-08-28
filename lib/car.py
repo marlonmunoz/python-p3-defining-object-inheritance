d0d4d15a898fcf2f03d0c9c9a365499fcef3bf97
Marlon Munoz <marlon@marlonshade.com>
2024-08-28 13:58:34 -0400

    lab completed

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,13 @@
 from vehicle import Vehicle
+# from car import Car 
+
 
 class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
+
+print('__bases__     ',Car.__bases__)
+print('int.__bases__ ',int.__bases__)
+print('__class__     ',Car.__class__)
+print('_class__      ',int.__class__)
